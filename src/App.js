5cf9b9debb6dfab99de84b7b8b446cb13be44e61
Greber <119353031+GreberPl@users.noreply.github.com>
2024-02-04 20:17:56 +0100

    no początek kurde

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Piraty from './data/fkl';
+import Header from './components/header.js';
+import Footer from './components/footer.js';
+import Main from './components/main.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+        <Main />  
+        <Footer />      
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>OnePiecowo</h1>
+        </header>
+    );
+}
