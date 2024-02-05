e7096de95aeeb11bbf6eec2d530bb552d84b27c5
Greber <119353031+GreberPl@users.noreply.github.com>
2024-02-05 12:48:45 +0100

    Patch

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 export default function Header() {
     return (
         <header>
-            <h1>OnePiecowo</h1>
+            <h1>OnePiecdle</h1>
         </header>
     );
 }
